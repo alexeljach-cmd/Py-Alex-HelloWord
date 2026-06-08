be4398f09d4c7124f19f12d050267f8821d1ceda
Alex Eljach <alexeljach@gmail.com>
2026-06-08 16:14:17 -0500

    Agregue Nuevas clases de Ejemplo

---
+++ Py-helloWord-Alex/Py-ciclos.py
@@ -0,0 +1,7 @@
+print("this will execute first")
+
+for _ in range(3):
+    print("this will execute three times")
+    print("this will execute also three times")
+
+print("this will execute last")
--- Py-helloWord-Alex/Turtle.py
+++ Py-helloWord-Alex/Turtle.py
@@ -1,6 +1,8 @@
 import turtle
 wn = turtle.Screen()
+wn.bgcolor("lightgreen")
 alex = turtle.Turtle()
+alex.color("blue")
 alex.forward(150)
 alex.left(90)
 alex.forward(75)
